19af28716bf86912480b7a22b7b5346976257074
liuyao <liuyao@ejoy.com>
2019-12-27 10:55:28 +0800

    添加redis布隆

--- spring-distributed-sample/src/test/java/com/github/flyingglass/distributed/SpringDistributedSampleApplicationTests.java
+++ spring-distributed-sample/src/test/java/com/github/flyingglass/distributed/SpringDistributedSampleApplicationTests.java
@@ -8,6 +8,7 @@ import orestes.bloomfilter.FilterBuilder;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -20,6 +21,10 @@ import java.util.concurrent.TimeUnit;
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 class SpringDistributedSampleApplicationTests {
+
+    @Resource
+    RedisProperties redisProperties;
+
 
     @Resource
     StringRedisTemplate stringRedisTemplate;
@@ -57,7 +62,8 @@ class SpringDistributedSampleApplicationTests {
         bf.add("Springboot");
 
         log.info(
-                "{}, {}, {}, {}",
+                "{}:{}, {}, {}, {}",
+                "布隆过滤器",
                 bf.contains("Just"),
                 bf.contains("Springboot +Mybatis-Plus + 多数据源 + Druid + Redis二级缓存"),
                 bf.contains("Springboot"),
@@ -66,7 +72,31 @@ class SpringDistributedSampleApplicationTests {
     }
 
     @Test
-    void a02_redis_distributed_lock() {
+    void a02_redisBloomFilter() {
+//        Redis布隆过滤器
+        FilterBuilder builder = new FilterBuilder(1000, 0.1)
+                .redisHost(redisProperties.getHost())
+                .redisPort(redisProperties.getPort());
+
+
+        BloomFilter<String> bf = builder.buildBloomFilter();
+
+        bf.add("Just");
+        bf.add("Springboot +Mybatis-Plus + 多数据源 + Druid + Redis二级缓存");
+        bf.add("Springboot");
+
+        log.info(
+                "{}:{}, {}, {}, {}",
+                "Redis布隆过滤器",
+                bf.contains("Just"),
+                bf.contains("Springboot +Mybatis-Plus + 多数据源 + Druid + Redis二级缓存"),
+                bf.contains("Springboot"),
+                bf.contains("404")
+        );
+    }
+
+    @Test
+    void a03_redisDistributedLock() {
         // TODO 测试不通过，直接使用redisson
         RedisSampleLock lock = new RedisSampleLock(stringRedisTemplate, "lock", 10000L);
         log.info("{}", lock.tryLock());
